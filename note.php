dacba1dca7c2bd9ecba05db09437cc6db597d2c6
4260snow <75683039+4260snow@users.noreply.github.com>
2023-01-21 06:42:58 +0300

    Update note.php

--- note.php
+++ note.php
@@ -11,7 +11,7 @@
 			</p>
 			<div class="d-flex justify-content-between align-items-center">
 				<div class="btn-group">
-					<button type="button" class="btn btn-sm btn-outline-secondary">Удалить</button>
+					<button id="btnSend" type="button" class="btn btn-sm btn-outline-secondary">Удалить</button>
 				</div>
 				<!--<small class="text-muted">Количество просмотров</small>-->
 			</div>
